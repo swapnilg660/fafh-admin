f1998ff085c80e6768aad6d6c767634c272a8962
Shounak Joshi <61938398+stickybit-shounak@users.noreply.github.com>
2024-04-28 14:54:54 +0530

    Update config.js - Updated PRD base URL

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
  //export const BASE_URL = "http://192.168.0.102:5000";
 
 //Prod URL
-export const BASE_URL = "https://fafh-backend.onrender.com";
+export const BASE_URL = "https://fafh-backend-app-1afeb5c32da1.herokuapp.com/";
